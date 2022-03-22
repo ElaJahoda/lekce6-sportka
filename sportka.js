d52c2c3a8fa1408b94687eda35f359c131439379
Ela Jahoda <3la.jah0da@gmail.com>
2022-03-22 19:57:54 +0100

    ok?

--- sportka.js
+++ sportka.js
@@ -1,32 +1,8 @@
-// Udělej losování klasické Sportky
-
-
-
-// <span class="cislo">8</span>
-// ... který pak přidej dovnitř prvku :
-
 
 let osudi = [];
-
-for (let i = 1; i <= 49; i = i + 1) {
-    // Máš osudí čísel od 1 do 49
-    osudi.push(i);
-}
-
-// Náhodně z tohoto osudí vyber 7 čísel
 let tazenaCisla = [];
 
-for (let i = 0; i < 7; i++) {
-    let vyherniIndex = Math.floor(Math.random() * osudi.length;
-    let vyherniCislo = osudi[vyherniCislo];
-// Žádné číslo se nesmí v tahu opakovat (je vyjmuté z osudí, takže už ho nemůžeš znovu vylosovat)
-    tazenaCisla.push(vyherniCislo);
-    osudi.splice(vyherniIndex, 1);
-}
-
-// Z každého vylosovaného čísla vygeneruj následující HTML kód: ...
 let vyherniCisla = document.querySelector('#vyherni-cisla');
 
 for (let j = 0; j < tazenaCisla.length; j++) {
-    vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[j] + '</span>';
-}
+    vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[j] + '</span>';}
